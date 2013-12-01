8c2645adb7fd4aeab47fe58dc49330d258d990ac
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2013-12-01 19:56:35 +0100

    Added connection to a label

--- main.py
+++ main.py
@@ -6,32 +6,17 @@ login = {}
 f = open('login.conf', 'r')
 login['username'] = f.readline().rstrip('\n')
 login['password'] = f.readline().rstrip('\n')
+login['label'] = f.readline().rstrip('\n')
 f.close()
 
-print login
+#get the mails that are in the label (ordered by id)
+idList = mmplib.getLabelMails(login)
 
-try:
-	mail = imaplib.IMAP4_SSL('imap.gmail.com')
-	mail.login(login['username'],login['password'])
-	pass
-except mail.error:
-	raise mail.error, "Connection Error!"
-else: # if connection not succeeds imap raises an error, else it prints 
-	print "Connected."
-
-mail.select('INBOX')
-
-result, ids = mail.search(None, "ALL") #result should be 'OK'
-idList = ids[0].split() # ids[0] is a space separated string
-
+#fetch'em
 for i in range(0,len(idList)):
-	#fetching all emails by id
-	body = mail.fetch(idList[-i-1], "(UID BODY[TEXT])") #starting from last email
-	rawEmail = body[0][1]
+	
+	rawEmail = mail.fetch(idList[i], "(UID BODY[TEXT])")
 
 	#here goes the template parsing, the matching and the mail sending
-	#functions for that are in parselib
-	#parsed messages should be deleted?
+	#and the archiviation too
 
-mail.close()
-mail.logout()
--- mmplib.py
+++ mmplib.py
@@ -1,4 +1,26 @@
 from lxml import etree
+import imaplib
+
+def getLabelMails(login):
+	try:
+		mail = imaplib.IMAP4_SSL('imap.gmail.com')
+		mail.login(login['username'],login['password'])
+		pass
+	except mail.error:
+		raise(mail.error, "Connection Error!")
+	else: # if connection not succeeds imap raises an error, else it prints 
+		print("Connected.")
+
+	mail.select(login['label']) #after login, it selects the label specified in config
+
+	#parse the folder and get the emails by id
+	result, ids = mail.search(None, "ALL") #result should be 'OK'
+	idList = ids[0].split() # ids[0] is a space separated string
+
+	mail.close()
+	mail.logout()
+
+	return idList
 
 def parseTemp(templatePath):
 	#doc = etree.parse(f)
